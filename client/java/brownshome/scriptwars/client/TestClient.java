c56ffc3556360d16299c3d8af9f42a996edc72cb
James Brown <jamesphone180@gmail.com>
2017-03-22 17:22:06 +1300

    Small client changes

--- client/java/brownshome/scriptwars/client/TestClient.java
+++ client/java/brownshome/scriptwars/client/TestClient.java
@@ -4,7 +4,10 @@ public class TestClient {
 	public static void main(String[] args) throws InterruptedException { 
 		Network.connect(args.length == 0 ? 65536 : Integer.parseInt(args[0]), "localhost", 35565, "TESTING");
 
-		while(Network.nextTick() && Network.getByte() != 0) {		
+		while(Network.nextTick()) {
+			if(Network.getByte() == 0)
+				System.out.println("We died");
+			
 			//pro level strats
 			Network.sendByte((byte) (Math.random() * 2 + 1));
 			Network.sendByte((byte) (Math.random() * 4));
