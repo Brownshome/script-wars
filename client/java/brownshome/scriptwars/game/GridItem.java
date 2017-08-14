d4e3b84bfda711126026c7cb865210c32e3d5aa0
Liam T. Brand <liamtbrand@gmail.com>
2017-08-14 16:29:48 +1200

    Add javadoc for GridItem

--- client/java/brownshome/scriptwars/game/GridItem.java
+++ client/java/brownshome/scriptwars/game/GridItem.java
@@ -3,7 +3,19 @@ package brownshome.scriptwars.game;
 import brownshome.scriptwars.game.tanks.Coordinates;
 
 public interface GridItem {
+	/**
+	 * The code is the number that will be passed to the javascript engine
+	 * @return
+	 */
 	byte code();
+	/**
+	 * The starting position that the item will be at the beginning of interpolation
+	 * @return
+	 */
 	Coordinates start();
+	/**
+	 * The finishing position that the item will be at the end of interpolation
+	 * @return
+	 */
 	Coordinates end();
 }
